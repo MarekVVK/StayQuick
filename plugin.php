5fba7eba34cf951615037e852ac131c86abe3eee
MarekVVK <marek.stankovskij@vvk.lt>
2024-03-20 16:23:08 +0200

    New plugin

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+//new wordpress plugin description comment
+/*
+ * Plugin Name: MS Stay Quick
+ * Plugin URI: http://www.mystays.com/
+ * Description: This plugin is used to display the quick search form for MyStays.
+ * Version: 1.0
+ * Author: MyStays
+ * Author URI: http://www.mystays.com/
+ */
